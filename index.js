bb7bc0d02db49997ac78f539745ac817fb794414
Your Name <yourname@example.com>
2024-10-01 12:26:41 +0545

    ADDED A DELETE ALL BUTTON

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads()
+    renderLeads();
 }
+
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear();
+    myLeads = [];
+    renderLeads();
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
