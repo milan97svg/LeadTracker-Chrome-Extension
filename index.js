bdf438b47d2a5cb8d908e1c0d4307548a8ad9765
Your Name <yourname@example.com>
2024-10-01 11:16:02 +0545

    READY FOR DEPLOYMENT
    FINAL PHASES OF ADDING ICONS REMAINING & FINAL
    TOUCHES REMAINING

---
+++ index.js
@@ -0,0 +1,36 @@
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+})
+
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+
+        // listItems += "<li><a target= '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+
+        listItems += `
+            <li>
+                <a target= '_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
+
+        // ||
+        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        // ||
+        // const li = document.createElement("li");
+        // li.textContent = myLeads[i];
+        // ulEl.append(li);
+
+        ulEl.innerHTML = listItems;
+    }
+}
+
