6befde1fdbf7b73ba34261330bcfd1ffad1889cb
Your Name <yourname@example.com>
2024-10-02 11:34:47 +0545

    ADDED A BUTTON TO SAVE TABS FROM CHROME TO OUR LEAD TRACKER TO THE CHROME EXTENSIONS

--- index.js
+++ index.js
@@ -3,41 +3,56 @@ const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 const deleteBtn = document.querySelector("#delete-btn");
+const tabBtn = document.querySelector("#tab-btn");
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
 
-deleteBtn.addEventListener("dblclick", () => {
-    localStorage.clear();
-    myLeads = [];
-    renderLeads();
+tabBtn.addEventListener("click", () => {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(tabs)
+
+        myLeads.push(tabs[0].url)
+        localStorage.setItme("myLeads", JSON.stringify(myLeads))
+        render(myLeads);
+
+    })
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads();
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
 
         // listItems += "<li><a target= '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
 
         listItems += `
             <li>
-                <a target= '_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target= '_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `;
         ulEl.innerHTML = listItems;
     }
 }
+
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads);
+})
+
+
 
